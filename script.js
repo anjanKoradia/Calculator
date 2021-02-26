45fc4cb323e0c64b77f2319652697cacff65f3f1
Anjan Koradia <anjankoradia2101@gmail.com>
2021-02-26 12:31:48 +0530

    Calculator Created.

---
+++ script.js
@@ -0,0 +1,19 @@
+// Dark Mode
+let checkbox = document.querySelector('input[name="theme"]');
+checkbox.addEventListener("change", function () {
+  if (this.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+});
+
+// Current Time
+setInterval(function () {
+  var currentTime = new Date().toLocaleTimeString("en-US", {
+    hour12: true,
+    hour: "numeric",
+    minute: "numeric",
+  });
+  document.querySelector(".time").innerHTML = currentTime;
+}, 1000);
